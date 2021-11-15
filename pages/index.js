a7ab677a3289df9c131abdd309f428ef224555a5
Andrew Gurney <AGurney@FindMyPast.com>
2021-11-15 17:27:22 +0000

    Add rhyme length checker

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,27 @@ export default function Home() {
   const [initialPhrase, setInitialPhrase] = useState(sentence());
   const [responsePhrase, setResponsePhrase] = useState('');
 
+  const sanitisedInitialPhrase = initialPhrase.replace(/[^a-z ]/gi, '').trim();
+  const sanitisedResponsePhrase = responsePhrase
+    .replace(/[^a-z ]/gi, '')
+    .trim();
+
+  const initialWordCount =
+    sanitisedInitialPhrase === ''
+      ? 0
+      : sanitisedInitialPhrase.split(' ').length;
+  const responseWordCount =
+    sanitisedResponsePhrase === ''
+      ? 0
+      : sanitisedResponsePhrase.split(' ').length;
+
   return (
     <>
       <Head>
-        <title>'Storm Kelpie'</title>
+        <title>Storm Kelpie</title>
       </Head>
       <section>
-        <h2>{'Blue Man of the Minch Rhyme:'}</h2>
+        <h2>Blue Man of the Minch</h2>
         <p>{initialPhrase}</p>
       </section>
       <section>
@@ -23,7 +37,22 @@ export default function Home() {
           type="text"
           value={responsePhrase}
           onChange={(event) => setResponsePhrase(event.target.value)}
+          placeholder={'Respond with a rhyme of the same number of words'}
         />
+      </section>
+      <section>
+        <h2>Score</h2>
+        <ul>
+          <li>Must have same word count</li>
+          <ul>
+            <li>Initial word count: {initialWordCount}</li>
+            <li>Response word count: {responseWordCount}</li>
+            <li>
+              Same length:{' '}
+              {initialWordCount === responseWordCount ? 'yes' : 'no'}
+            </li>
+          </ul>
+        </ul>
       </section>
     </>
   );
