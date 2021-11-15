7d8ef1e7bfa0a059edc53af1901d15a6b2711318
Andrew Gurney <AGurney@FindMyPast.com>
2021-11-15 17:53:22 +0000

    Add rhyme checker

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-import { sentence } from 'txtgen';
 import { useState } from 'react';
+import { sentence } from 'txtgen';
+import { dictionary } from 'cmu-pronouncing-dictionary';
 
 export default function Home() {
   const [initialPhrase, setInitialPhrase] = useState(sentence());
@@ -21,6 +22,25 @@ export default function Home() {
     sanitisedResponsePhrase === ''
       ? 0
       : sanitisedResponsePhrase.split(' ').length;
+
+  const initialPhraseEndingWord = sanitisedInitialPhrase
+    .split(' ')
+    .slice(-1)[0];
+  const responsePhraseEndingWord = sanitisedResponsePhrase
+    .split(' ')
+    .slice(-1)[0];
+
+  const initialPhraseFinalWordPronunciation =
+    dictionary[initialPhraseEndingWord];
+  const responsePhraseFinalWordPronunciation =
+    dictionary[responsePhraseEndingWord];
+
+  const initialPhraseFinalWordRhyme =
+    initialPhraseFinalWordPronunciation &&
+    initialPhraseFinalWordPronunciation.split(' ').slice(-2).join(' ');
+  const responsePhraseFinalWordRhyme =
+    responsePhraseFinalWordPronunciation &&
+    responsePhraseFinalWordPronunciation.split(' ').slice(-2).join(' ');
 
   return (
     <>
@@ -45,11 +65,52 @@ export default function Home() {
         <ul>
           <li>Must have same word count</li>
           <ul>
-            <li>Initial word count: {initialWordCount}</li>
-            <li>Response word count: {responseWordCount}</li>
+            <li>
+              Initial word count: <em>{initialWordCount}</em>
+            </li>
+            <li>
+              Response word count: <em>{responseWordCount}</em>
+            </li>
             <li>
               Same length:{' '}
-              {initialWordCount === responseWordCount ? 'yes' : 'no'}
+              <em>{initialWordCount === responseWordCount ? 'yes' : 'no'}</em>
+            </li>
+          </ul>
+          <li>Must rhyme</li>
+          <ul>
+            <li>
+              {'Final word in initial phrase: '}
+              <em>{initialPhraseEndingWord}</em>
+            </li>
+            <ul>
+              <li>
+                <a
+                  target="_blank"
+                  href={'http://www.speech.cs.cmu.edu/cgi-bin/cmudict'}
+                >{`CMU pronouncing dictionary ending symbols: `}</a>
+                <em>{initialPhraseFinalWordRhyme || 'unknown'}</em>
+              </li>
+            </ul>
+            <li>
+              {'Final word in response phrase: '}
+              <em>{responsePhraseEndingWord || 'none'}</em>
+            </li>
+            <ul>
+              <li>
+                <a
+                  target="_blank"
+                  href={'http://www.speech.cs.cmu.edu/cgi-bin/cmudict'}
+                >{`CMU pronouncing dictionary ending symbols: `}</a>
+                <em>{responsePhraseFinalWordRhyme || 'unknown'}</em>
+              </li>
+            </ul>
+            <li>
+              {'Rhyme: '}
+              <em>
+                {initialPhraseFinalWordRhyme === responsePhraseFinalWordRhyme
+                  ? 'yes'
+                  : 'no'}
+              </em>
             </li>
           </ul>
         </ul>
