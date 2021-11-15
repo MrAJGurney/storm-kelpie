8e27a99929fb2c206258a9051a54e2ab072cae74
Andrew Gurney <AGurney@FindMyPast.com>
2021-11-15 16:53:17 +0000

    Add input textbox
    Additional changes were required to prevent the rhyme changing on text
    entry

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,11 @@ import Head from 'next/head';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import { sentence } from 'txtgen';
+import { useState } from 'react';
 
 export default function Home() {
-  const phraseToRhyme = sentence();
+  const [initialPhrase, setInitialPhrase] = useState(sentence());
+  const [responsePhrase, setResponsePhrase] = useState('');
 
   return (
     <>
@@ -13,7 +15,15 @@ export default function Home() {
       </Head>
       <section>
         <h2>{'Blue Man of the Minch Rhyme:'}</h2>
-        <p>{phraseToRhyme}</p>
+        <p>{initialPhrase}</p>
+      </section>
+      <section>
+        <h2>Your Response</h2>
+        <textarea
+          type="text"
+          value={responsePhrase}
+          onChange={(event) => setResponsePhrase(event.target.value)}
+        />
       </section>
     </>
   );
