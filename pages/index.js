f2332f169fd2be772c2da6e79f85e6ba904d5348
Andrew Gurney <AGurney@FindMyPast.com>
2021-11-17 17:44:22 +0000

    Add basic styling

--- pages/index.js
+++ pages/index.js
@@ -82,103 +82,122 @@ export default function Home() {
       <Head>
         <title>Storm Kelpie</title>
       </Head>
-      <section>
-        <h2>Blue Man of the Minch</h2>
-        <p>{initialPhrase}</p>
-      </section>
-      <section>
-        <h2>Your Response</h2>
-        <textarea
-          type="text"
-          value={responsePhrase}
-          onChange={(event) => setResponsePhrase(event.target.value)}
-          placeholder={'complete the rhyme here'}
-        />
-      </section>
-      <section>
-        <h2>Score</h2>
-        <ul>
-          <li>Must have same word count</li>
-          <ul>
-            <li>
-              Initial word count: <em>{initialWordCount}</em>
-            </li>
-            <li>
-              Response word count: <em>{responseWordCount}</em>
-            </li>
-            <li>
-              <b>
-                {'Same length: '}
-                <em>{responsePhraseIsSameLength ? 'yes' : 'no'}</em>
-              </b>
-            </li>
-          </ul>
-          <li>Must use unique words</li>
-          <ul>
-            <li>
-              Repeated words:{' '}
-              <em>
-                {duplicateWordsInResponse.length > 0
-                  ? duplicateWordsInResponse.join(', ')
-                  : 'none'}
-              </em>
-            </li>
-            <li>
-              <b>
-                {'Unique words: '}
-                <em>{responsePhraseHasOnlyUniqueWords ? 'yes' : 'no'}</em>
-              </b>
-            </li>
-          </ul>
-          <li>Must rhyme</li>
-          <ul>
-            <li>
-              {'Final word in initial phrase: '}
-              <em>{initialPhraseEndingWord}</em>
-            </li>
+      <div
+        style={{
+          backgroundColor: 'black',
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: 'white',
+            padding: '10px',
+          }}
+        >
+          <section>
+            <h2>Blue Man of the Minch</h2>
+            <p>{initialPhrase}</p>
+          </section>
+          <section>
+            <h2>Your Response</h2>
+            <textarea
+              style={{ resize: 'none', width: '100%' }}
+              type="text"
+              value={responsePhrase}
+              onChange={(event) => setResponsePhrase(event.target.value)}
+              placeholder={'complete the rhyme here'}
+            />
+          </section>
+          <section>
+            <h2>Score</h2>
             <ul>
-              <li>
-                <a
-                  target="_blank"
-                  href={'http://www.speech.cs.cmu.edu/cgi-bin/cmudict'}
-                >{`CMU pronouncing dictionary ending symbols: `}</a>
-                <em>{initialPhraseFinalWordRhyme || 'unknown'}</em>
-              </li>
+              <li>Must have same word count</li>
+              <ul>
+                <li>
+                  Initial word count: <em>{initialWordCount}</em>
+                </li>
+                <li>
+                  Response word count: <em>{responseWordCount}</em>
+                </li>
+                <li>
+                  <b>
+                    {'Same length: '}
+                    <em>{responsePhraseIsSameLength ? 'yes' : 'no'}</em>
+                  </b>
+                </li>
+              </ul>
+              <li>Must use unique words</li>
+              <ul>
+                <li>
+                  Repeated words:{' '}
+                  <em>
+                    {duplicateWordsInResponse.length > 0
+                      ? duplicateWordsInResponse.join(', ')
+                      : 'none'}
+                  </em>
+                </li>
+                <li>
+                  <b>
+                    {'Unique words: '}
+                    <em>{responsePhraseHasOnlyUniqueWords ? 'yes' : 'no'}</em>
+                  </b>
+                </li>
+              </ul>
+              <li>Must rhyme</li>
+              <ul>
+                <li>
+                  {'Final word in initial phrase: '}
+                  <em>{initialPhraseEndingWord}</em>
+                </li>
+                <ul>
+                  <li>
+                    <a
+                      target="_blank"
+                      href={'http://www.speech.cs.cmu.edu/cgi-bin/cmudict'}
+                    >{`CMU pronouncing dictionary ending symbols: `}</a>
+                    <em>{initialPhraseFinalWordRhyme || 'unknown'}</em>
+                  </li>
+                </ul>
+                <li>
+                  {'Final word in response phrase: '}
+                  <em>{responsePhraseEndingWord || 'none'}</em>
+                </li>
+                <ul>
+                  <li>
+                    <a
+                      target="_blank"
+                      href={'http://www.speech.cs.cmu.edu/cgi-bin/cmudict'}
+                    >{`CMU pronouncing dictionary ending symbols: `}</a>
+                    <em>{responsePhraseFinalWordRhyme || 'unknown'}</em>
+                  </li>
+                </ul>
+                <li>
+                  <b>
+                    {'Rhyme: '}
+                    <em>{responsePhraseIsRhyme ? 'yes' : 'no'}</em>
+                  </b>
+                </li>
+              </ul>
             </ul>
-            <li>
-              {'Final word in response phrase: '}
-              <em>{responsePhraseEndingWord || 'none'}</em>
-            </li>
-            <ul>
-              <li>
-                <a
-                  target="_blank"
-                  href={'http://www.speech.cs.cmu.edu/cgi-bin/cmudict'}
-                >{`CMU pronouncing dictionary ending symbols: `}</a>
-                <em>{responsePhraseFinalWordRhyme || 'unknown'}</em>
-              </li>
-            </ul>
-            <li>
-              <b>
-                {'Rhyme: '}
-                <em>{responsePhraseIsRhyme ? 'yes' : 'no'}</em>
-              </b>
-            </li>
-          </ul>
-        </ul>
-      </section>
-      <section>
-        <h2>
-          {responsePhraseIsSameLength &&
-          responsePhraseHasOnlyUniqueWords &&
-          responsePhraseIsRhyme
-            ? '🚣 success 🚣'
-            : '💀 sunk 💀'}
-        </h2>
-        <button onClick={() => setInitialPhrase(sentence())}>
-          generate new phrase
-        </button>
-      </section>
+          </section>
+          <section>
+            <h2>
+              {responsePhraseIsSameLength &&
+              responsePhraseHasOnlyUniqueWords &&
+              responsePhraseIsRhyme
+                ? '🚣 success 🚣'
+                : '💀 sunk 💀'}
+            </h2>
+            <button onClick={() => setInitialPhrase(sentence())}>
+              generate new phrase
+            </button>
+          </section>
+        </div>
+      </div>
     </>
   );
 }
